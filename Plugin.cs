d766e0857d9deab78a01e118994da928ade9771b
Yeechan Lu <wz.bluesnow@gmail.com>
2014-03-23 04:28:23 +0800

    Finish 1.0.0

--- Plugin.cs
+++ Plugin.cs
@@ -1,8 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
-using Newtonsoft.Json;
+using System.Windows.Media.Imaging;
 using S = Switcheroo;
 
 namespace Wox.Plugin.Switcheroo
@@ -15,6 +16,26 @@ namespace Wox.Plugin.Switcheroo
         {
             this.context = context;
             S.CoreStuff.Initialize();
+        }
+
+        public String IconImageDataUri(S.AppWindow self)
+        {
+            var key = "IconImageDataUri-" + self.HWnd;
+            var iconImageDataUri = System.Runtime.Caching.MemoryCache.Default.Get(key) as String; ;
+            if (iconImageDataUri == null)
+            {
+                var iconImage = self.IconImage;
+                using (MemoryStream memoryStream = new MemoryStream())
+                {
+                    BitmapEncoder encoder = new PngBitmapEncoder();
+                    encoder.Frames.Add(BitmapFrame.Create(iconImage));
+                    encoder.Save(memoryStream);
+                    var b64String = Convert.ToBase64String(memoryStream.ToArray());
+                    iconImageDataUri = "data:image/png;base64," + b64String;
+                    System.Runtime.Caching.MemoryCache.Default.Add(key, iconImageDataUri, DateTimeOffset.Now.AddHours(1));
+                }
+            }
+            return iconImageDataUri;
         }
 
         public List<Result> Query(Query query)
@@ -31,10 +52,19 @@ namespace Wox.Plugin.Switcheroo
                 {
                     Title = o.AppWindow.Title,
                     SubTitle = o.AppWindow.ProcessTitle,
+                    IcoPath = IconImageDataUri(o.AppWindow),
                     Action = con =>
                     {
                         context.HideApp();
-                        o.AppWindow.SwitchTo();
+                        if (con.SpecialKeyState.CtrlPressed)
+                        {
+                            o.AppWindow.PostClose();
+                            o.AppWindow.SwitchTo();
+                        }
+                        else
+                        {
+                            o.AppWindow.SwitchTo();
+                        }
                         return true;
                     }
                 };
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 [Wox](http://getwox.com) Plugin: Switcheroo
 ===========================================
+![image](https://f.cloud.github.com/assets/158528/2491834/eefa9062-b1ff-11e3-95ec-0e4e06ef9776.png)
+
+[![Build status](https://ci.appveyor.com/api/projects/status/8n8q2rm7b8e1m269)](https://ci.appveyor.com/project/orzFly/wox-plugin-switcheroo)
+
+Switcheroo
+----------
+[Switcheroo](http://www.switcheroo.io/) is for anyone who spends more time using a keyboard than a mouse. Instead of alt-tabbing through a (long) list of open windows, Switcheroo allows you to quickly switch to any window by typing in just a few characters of its title. It's inspired by Emacs's IDO mode buffer switching and JetBrains' ReSharper.
